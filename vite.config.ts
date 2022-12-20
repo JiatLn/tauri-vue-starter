f1c0f20c1ed68b2fe7ef326b523185ae6c71a7e6
林佳涛 <lne128gene980@163.com>
2022-12-20 17:27:16 +0800

    deps: add unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,10 @@ export default defineConfig({
         'src/composables',
         'src/stores',
       ],
+    }),
+    // https://github.com/antfu/unplugin-vue-components
+    Components({
+      dts: 'src/types/components.d.ts',
     }),
   ],
 
